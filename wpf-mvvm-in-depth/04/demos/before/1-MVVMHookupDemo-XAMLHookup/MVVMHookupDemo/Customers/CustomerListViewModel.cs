4c128914b2c3b98ca7c939e22176cf88c5dcaaab
Muhammad Noman <Muhammad.Noman@bentley.com>
2020-09-20 18:49:05 +0500

    finised

---
+++ wpf-mvvm-in-depth/04/demos/before/1-MVVMHookupDemo-XAMLHookup/MVVMHookupDemo/Customers/CustomerListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MVVMHookupDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+
+namespace MVVMHookupDemo.Customers
+{
+    public class CustomerListViewModel
+    {
+        ICustomersRepository r = new CustomersRepository();
+        public CustomerListViewModel() {
+            if (DesignerProperties.GetIsInDesignMode(
+                new System.Windows.DependencyObject())) return;
+               
+            Customers = new ObservableCollection<Customer>( r.GetCustomersAsync().Result);
+        }
+        public ObservableCollection<Customer> Customers { get; set; }
+    }
+}
